b4aecdd91f2010bcf29901b61831809c50c5bd93
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 19:25:17 +0530

    Update ZoneCard.js

--- dashboard/src/components/ZoneCard.js
+++ dashboard/src/components/ZoneCard.js
@@ -1,1 +1,67 @@
+function ZoneCard({zone})
+{
+    return (
+        <div
+            style={{
+                border:"1px solid #cccccc",
+                marginBottom:"15px",
+                padding:"15px",
+                borderRadius:"8px"
+            }}
+        >
+            <h3>
+                Zone {zone.zone_id}
+            </h3>
 
+            <p>
+                Temperature:
+                {" "}
+                {zone.temperature}
+                °C
+            </p>
+
+            <p>
+                Humidity:
+                {" "}
+                {zone.humidity}
+                %
+            </p>
+
+            <p>
+                Light:
+                {" "}
+                {zone.light}
+                lux
+            </p>
+
+            <p>
+                Occupied:
+                {" "}
+                {zone.occupied ? "Yes" : "No"}
+            </p>
+
+            <p>
+                Comfort Score:
+                {" "}
+                {zone.comfort_score}
+            </p>
+
+            <p>
+                Fan Speed:
+                {" "}
+                {zone.fan_speed}
+                %
+            </p>
+
+            <p>
+                Brightness:
+                {" "}
+                {zone.brightness}
+                %
+            </p>
+
+        </div>
+    );
+}
+
+export default ZoneCard;
