f404249aec763812721c5e1f564a57ceded770d7
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 19:25:00 +0530

    Update StatisticsCard.js

--- dashboard/src/components/StatisticsCard.js
+++ dashboard/src/components/StatisticsCard.js
@@ -1,1 +1,29 @@
+function StatisticsCard({stats})
+{
+    return (
+        <div
+            style={{
+                border:"1px solid gray",
+                padding:"10px",
+                marginBottom:"20px"
+            }}
+        >
+            <h2>System Statistics</h2>
 
+            <p>
+                Active Zones:
+                {" "}
+                {stats.active_zones}
+            </p>
+
+            <p>
+                Average Comfort:
+                {" "}
+                {stats.average_comfort}
+            </p>
+
+        </div>
+    );
+}
+
+export default StatisticsCard;
