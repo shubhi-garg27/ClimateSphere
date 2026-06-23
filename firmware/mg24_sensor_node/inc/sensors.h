8e124b028fdd7f14ff8a1712d9380aa3c62b4a33
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:44:59 +0530

    Update sensors.h

--- firmware/mg24_sensor_node/inc/sensors.h
+++ firmware/mg24_sensor_node/inc/sensors.h
@@ -1,1 +1,16 @@
+#ifndef SENSORS_H
+#define SENSORS_H
 
+typedef struct
+{
+    float temperature;
+    float humidity;
+    float lux;
+    int occupied;
+} sensor_data_t;
+
+void sensors_init(void);
+
+sensor_data_t read_all_sensors(void);
+
+#endif
