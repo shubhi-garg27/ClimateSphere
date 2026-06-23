987ee26af0a42110a7665fd05247c480073b5559
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 19:50:02 +0530

    Update predict.py

--- ai/predict.py
+++ ai/predict.py
@@ -1,1 +1,37 @@
 
+import tensorflow as tf
+import numpy as np
+
+interpreter = tf.lite.Interpreter(
+    model_path=
+    "model/comfort_model.tflite"
+)
+
+interpreter.allocate_tensors()
+
+input_details = \
+    interpreter.get_input_details()
+
+output_details = \
+    interpreter.get_output_details()
+
+sample = np.array(
+    [[28,60,300,1]],
+    dtype=np.float32
+)
+
+interpreter.set_tensor(
+    input_details[0]['index'],
+    sample
+)
+
+interpreter.invoke()
+
+prediction = interpreter.get_tensor(
+    output_details[0]['index']
+)
+
+print(
+    "Predicted Comfort Score:",
+    prediction[0][0]
+)
