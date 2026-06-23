f10666cec648995b248dd05587440e6d4bb18b20
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:46:19 +0530

    Update thread_comm.c

--- firmware/mg24_sensor_node/src/thread_comm.c
+++ firmware/mg24_sensor_node/src/thread_comm.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include "thread_comm.h"
 
+void thread_init(void)
+{
+}
+
+void thread_send(sensor_data_t data)
+{
+    printf(
+        "{\"temperature\":%.2f,"
+        "\"humidity\":%.2f,"
+        "\"lux\":%.2f,"
+        "\"occupied\":%d}\n",
+        data.temperature,
+        data.humidity,
+        data.lux,
+        data.occupied
+    );
+}
