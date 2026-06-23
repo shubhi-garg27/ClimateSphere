a88afe0dbae268f8d5554b0b3be6f11c3de0f2dd
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:51:28 +0530

    Update comfort_engine.h

--- firmware/mg26_edge_controller/inc/comfort_engine.h
+++ firmware/mg26_edge_controller/inc/comfort_engine.h
@@ -1,1 +1,16 @@
+#ifndef COMFORT_ENGINE_H
+#define COMFORT_ENGINE_H
 
+typedef struct
+{
+    int fan_speed;
+    int brightness;
+    int vent_angle;
+} comfort_output_t;
+
+comfort_output_t evaluate_zone(
+        float temp,
+        float humidity,
+        int occupied);
+
+#endif
