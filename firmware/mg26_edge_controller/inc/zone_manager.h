338bd7b37c713b274d8c7af8fd8ccf39095a9668
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:59:26 +0530

    Update zone_manager.h

--- firmware/mg26_edge_controller/inc/zone_manager.h
+++ firmware/mg26_edge_controller/inc/zone_manager.h
@@ -1,1 +1,32 @@
 
+#ifndef ZONE_MANAGER_H
+#define ZONE_MANAGER_H
+
+#include "climate_model.h"
+
+#define MAX_ZONES 5
+
+typedef struct
+{
+    int zone_id;
+
+    climate_input_t sensor_data;
+
+    climate_prediction_t prediction;
+
+} zone_t;
+
+void zone_manager_init(void);
+
+void update_zone(
+        int zone_id,
+        climate_input_t input);
+
+zone_t get_zone(
+        int zone_id);
+
+void evaluate_all_zones(void);
+
+int get_active_zone_count(void);
+
+#endif
