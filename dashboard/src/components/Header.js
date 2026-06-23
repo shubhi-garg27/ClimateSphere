a75a955d788496c72691679017cc09b9d1c3c908
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 19:24:10 +0530

    Update Header.js

--- dashboard/src/components/Header.js
+++ dashboard/src/components/Header.js
@@ -1,1 +1,21 @@
 
+function Header()
+{
+    return (
+        <div>
+
+            <h1>
+                ClimateSphere Dashboard
+            </h1>
+
+            <p>
+                Personalized Indoor Comfort Monitoring
+            </p>
+
+            <hr/>
+
+        </div>
+    );
+}
+
+export default Header;
