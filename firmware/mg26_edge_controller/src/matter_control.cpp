3891e3751ba0a084605a46049877b1df84eef941
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:52:32 +0530

    Update matter_control.cpp

--- firmware/mg26_edge_controller/src/matter_control.cpp
+++ firmware/mg26_edge_controller/src/matter_control.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+void set_fan_speed(int speed)
+{
+    std::cout << "Fan: " << speed << std::endl;
+}
+
+void set_light_level(int level)
+{
+    std::cout << "Light: " << level << std::endl;
+}
+
+void set_vent_angle(int angle)
+{
+    std::cout << "Vent: " << angle << std::endl;
+}
