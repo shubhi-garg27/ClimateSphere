8b6c7bf63f75b9ff60317f04bd51b307e2254481
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 19:38:12 +0530

    Update install.sh

--- raspberry_pi/otbr/install.sh
+++ raspberry_pi/otbr/install.sh
@@ -1,1 +1,20 @@
 
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y \
+docker.io \
+git \
+curl \
+avahi-daemon
+
+git clone \
+https://github.com/openthread/ot-br-posix.git
+
+cd ot-br-posix
+
+sudo ./script/bootstrap
+
+INFRA_IF_NAME=wlan0 \
+sudo ./script/setup
