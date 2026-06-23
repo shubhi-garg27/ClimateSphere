fb9061f89581c0ac3f93fa8977ed4d0faa3c9720
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 19:40:52 +0530

    Update devices.py

--- raspberry_pi/matter/devices.py
+++ raspberry_pi/matter/devices.py
@@ -1,1 +1,31 @@
+class Fan:
 
+    def __init__(self):
+
+        self.speed = 0
+
+    def set_speed(self, speed):
+
+        self.speed = speed
+
+
+class Light:
+
+    def __init__(self):
+
+        self.level = 0
+
+    def set_level(self, level):
+
+        self.level = level
+
+
+class Vent:
+
+    def __init__(self):
+
+        self.angle = 0
+
+    def set_angle(self, angle):
+
+        self.angle = angle
