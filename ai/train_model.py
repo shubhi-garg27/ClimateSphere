3027dbe378d2b926cef20a806355daf3a3586c2e
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 19:49:17 +0530

    Update train_model.py

--- ai/train_model.py
+++ ai/train_model.py
@@ -1,1 +1,67 @@
+import pandas as pd
+import tensorflow as tf
 
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv(
+    "comfort_dataset.csv"
+)
+
+X = data[
+    [
+        "temperature",
+        "humidity",
+        "light",
+        "occupied"
+    ]
+]
+
+y = data[
+    "comfort_score"
+]
+
+X_train, X_test, y_train, y_test = \
+    train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42
+    )
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(
+        16,
+        activation='relu',
+        input_shape=(4,)
+    ),
+
+    tf.keras.layers.Dense(
+        8,
+        activation='relu'
+    ),
+
+    tf.keras.layers.Dense(
+        1
+    )
+])
+
+model.compile(
+    optimizer='adam',
+    loss='mse',
+    metrics=['mae']
+)
+
+model.fit(
+    X_train,
+    y_train,
+    epochs=100,
+    verbose=1
+)
+
+model.save(
+    "model/comfort_model.h5"
+)
+
+print(
+    "Model training complete."
+)
