ce942a6b26b43cea7c0d47ba20d920dd8e211e03
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:54:37 +0530

    Update climate_model.h

--- firmware/mg26_edge_controller/inc/climate_model.h
+++ firmware/mg26_edge_controller/inc/climate_model.h
@@ -1,1 +1,24 @@
+#ifndef CLIMATE_MODEL_H
+#define CLIMATE_MODEL_H
 
+typedef struct
+{
+    float temperature;
+    float humidity;
+    float light_level;
+    int occupied;
+} climate_input_t;
+
+typedef struct
+{
+    float comfort_score;
+    int recommended_fan_speed;
+    int recommended_brightness;
+} climate_prediction_t;
+
+void climate_model_init(void);
+
+climate_prediction_t predict_comfort(
+        climate_input_t input);
+
+#endif
