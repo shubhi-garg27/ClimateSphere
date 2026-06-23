6b5a0041553c984d4a9f097b1b8eec4bc6c6d649
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:45:30 +0530

    Update sensors.c

--- firmware/mg24_sensor_node/src/sensors.c
+++ firmware/mg24_sensor_node/src/sensors.c
@@ -1,1 +1,17 @@
+#include "sensors.h"
 
+void sensors_init(void)
+{
+}
+
+sensor_data_t read_all_sensors(void)
+{
+    sensor_data_t data;
+
+    data.temperature = 25.2;
+    data.humidity = 58.4;
+    data.lux = 420.0;
+    data.occupied = 1;
+
+    return data;
+}
