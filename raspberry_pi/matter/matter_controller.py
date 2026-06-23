3dfaeeb8785606c5bfb54d9e26baf55ce5b61d55
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 19:41:33 +0530

    Update matter_controller.py

--- raspberry_pi/matter/matter_controller.py
+++ raspberry_pi/matter/matter_controller.py
@@ -1,1 +1,32 @@
+from devices import *
 
+fan = Fan()
+
+light = Light()
+
+vent = Vent()
+
+
+def update_devices(
+        fan_speed,
+        brightness,
+        vent_angle):
+
+    fan.set_speed(
+        fan_speed
+    )
+
+    light.set_level(
+        brightness
+    )
+
+    vent.set_angle(
+        vent_angle
+    )
+
+    print(
+        "Matter Update:",
+        fan_speed,
+        brightness,
+        vent_angle
+    )
