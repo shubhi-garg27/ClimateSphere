da48ffba75f6a8bcddd7d0e08c110911bb5b6633
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:52:52 +0530

    Update main.cpp

--- firmware/mg26_edge_controller/src/main.cpp
+++ firmware/mg26_edge_controller/src/main.cpp
@@ -1,1 +1,27 @@
+#include "comfort_engine.h"
+#include "matter_control.h"
 
+int main()
+{
+    float temp = 28.0;
+
+    float humidity = 60.0;
+
+    int occupied = 1;
+
+    comfort_output_t result;
+
+    result =
+        evaluate_zone(
+            temp,
+            humidity,
+            occupied);
+
+    set_fan_speed(result.fan_speed);
+
+    set_light_level(result.brightness);
+
+    set_vent_angle(result.vent_angle);
+
+    return 0;
+}
