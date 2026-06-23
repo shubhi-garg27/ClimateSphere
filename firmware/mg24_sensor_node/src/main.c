70d54ed6c1e908b94e3cfc2df8e0bd2252f85d29
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:46:51 +0530

    Update main.c

--- firmware/mg24_sensor_node/src/main.c
+++ firmware/mg24_sensor_node/src/main.c
@@ -1,1 +1,19 @@
 
+#include "sensors.h"
+#include "thread_comm.h"
+
+int main(void)
+{
+    sensors_init();
+
+    thread_init();
+
+    while(1)
+    {
+        sensor_data_t data;
+
+        data = read_all_sensors();
+
+        thread_send(data);
+    }
+}
